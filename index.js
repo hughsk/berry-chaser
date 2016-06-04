c78c4661dac9fa7eed099b5aa4584f3f5725a6fa
Hugh Kennedy <hughskennedy@gmail.com>
2016-06-04 12:05:08 +1000

    whoop

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const canvas = document.body.appendChild(document.createElement('canvas'))
+const gl = require('gl-context')(canvas, render)
+const Fit = require('canvas-fit')
+
 const Geom = require('gl-geometry')
 const Shader = require('gl-shader')
 const Node = require('scene-tree')
 const glslify = require('glslify')
+
+function render () {
+  const width = canvas.width
+  const height = canvas.height
+
+  gl.viewport(0, 0, width, height)
+  gl.clearColor(0, 0, 0, 1)
+}
+
+window.addEventListener('resize', Fit(canvas), false)
