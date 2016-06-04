dc4181866ff538732dec7811c612fdab68b32f59
Hugh Kennedy <hughskennedy@gmail.com>
2016-06-04 13:39:23 +1000

    whoop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const canvas = document.body.appendChild(document.createElement('canvas'))
-const gl = require('gl-context')(canvas, render)
 const camera = require('canvas-orbit-camera')(canvas)
+const gl = require('gl-context')(canvas, tick)
 const Fit = require('canvas-fit')
 
 const perspective = require('gl-mat4/perspective')
@@ -10,6 +10,7 @@ const Node = require('scene-tree')
 const scene = Node()
 const proj = new Float32Array(16)
 const view = new Float32Array(16)
+const lights = []
 
 const getNodeList = scene.list(require('./node-sorter'))
 
@@ -17,8 +18,29 @@ scene.add(
   Node({ geom: geoms.sphere, shader: shaders.sphere }),
   Node({ geom: geoms.sphere, shader: shaders.sphere }),
   Node({ geom: geoms.sphere, shader: shaders.sphere }),
-  Node({ geom: geoms.sphere, shader: shaders.sphere })
+  Node({ geom: geoms.sphere, shader: shaders.sphere }),
+  Node({ light: [1, 0, 0] })
 )
+
+function tick () {
+  step()
+  render()
+}
+
+function step () {
+  const width = canvas.width
+  const height = canvas.height
+
+  camera.tick()
+  camera.view(view)
+  perspective(proj, Math.PI / 4, width / height, 0.1, 100)
+
+  scene.each(function (node) {
+    node.setPosition(Math.random(), Math.random(), Math.random())
+  })
+
+  scene.tick()
+}
 
 function render () {
   const width = canvas.width
@@ -28,19 +50,25 @@ function render () {
   gl.clearColor(0, 0, 0, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
 
-  camera.tick()
-  camera.view(view)
-  perspective(proj, Math.PI / 4, width / height, 0.1, 100)
-
   const nodes = getNodeList()
   var currShad = null
   var currGeom = null
+
+  lights.length = 0
+
+  for (var i = 0; i < nodes.length; i++) {
+    var node = nodes[i]
+    if (node.data.light) lights.push(node.data.light)
+  }
 
   for (var i = 0; i < nodes.length; i++) {
     var node = nodes[i]
     var data = node.data
     var geom = data.geom
     var shad = data.shader
+
+    if (!geom) continue
+    if (!shad) continue
 
     if (currGeom !== geom) {
       currGeom = geom
@@ -51,6 +79,7 @@ function render () {
       shad.bind()
       shad.uniforms.proj = proj
       shad.uniforms.view = view
+      shad.uniforms.lights = lights
     }
 
     shad.uniforms.model = node.modelMatrix
--- shaders/sphere.frag
+++ shaders/sphere.frag
@@ -1,5 +1,9 @@
 precision mediump float;
 
+#define LIGHT_COUNT 2
+uniform vec3 lightPos[LIGHT_COUNT];
+uniform vec3 lightCol[LIGHT_COUNT];
+
 void main() {
   gl_FragColor = vec4(1, 0, 1, 1);
 }
